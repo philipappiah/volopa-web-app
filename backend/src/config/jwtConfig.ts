4b625dc61159deaebec270276c74cb734693fbcf
philipappiah <pappiahkubi1@gmail.com>
2022-08-01 10:08:51 +0100

    extend refresh token

--- backend/src/config/jwtConfig.ts
+++ backend/src/config/jwtConfig.ts
@@ -3,5 +3,5 @@
 export const JWT_SECRET = "my-56-92-secret-king-secure--@45$.and-op34-long-voluminous"
 export const REFRESH_TOKEN_SECRET = "another-gsecret-permission-jellybe2$.granted-$$sd"
 export const JWT_EXPIRES_IN = "30d"
-export const REFRESH_TOKEN_EXPIRES_IN = "30d"
+export const REFRESH_TOKEN_EXPIRES_IN = "90d"
 export const COOKIE_EXPIRES_IN= 30
